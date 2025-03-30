caba9c434b3859b21bcdaf173bed1dc9a9b5b2e2
Creaous <68230846+Creaous@users.noreply.github.com>
2025-03-30 16:45:19 +1030

    fix: github workflow registry

--- src/redis.ts
+++ src/redis.ts
@@ -16,8 +16,8 @@ export const redisClient = createClient({
 	url: !isTestMode ? (env.REDIS_URL as string) : 'redis://localhost:1234'
 });
 
-// Create a new Redis client just for tokens using the duplicate method.
-export const tokenClient = redisClient.duplicate();
-
 // Listen for any errors that occur in the Redis client.
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+// Create a new Redis client just for tokens using the duplicate method.
+export const tokenClient = redisClient.duplicate();
